e3901037053618bd74b91f35d28e0aafb7e69ec3
Debbie925 <741278056@qq.com>
2017-06-26 14:29:18 +0800

    Update _team_.py

--- pick_a_National_GDP_4web/_team_/_team_.py
+++ pick_a_National_GDP_4web/_team_/_team_.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import csv
-with open('_team_.tsv', encoding='u8') as f:
+with open('_team_.tsv', encoding='utf8') as f:
     reader = csv.DictReader(f)
     print ('\t'.join(reader.fieldnames))
     for r in reader:
